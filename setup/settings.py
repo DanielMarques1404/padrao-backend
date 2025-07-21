1c116cfa01c37b0acc913b14b1cbdc96d1be3b76
Daniel Marques <146373718+DanielMarques1404@users.noreply.github.com>
2025-07-20 21:02:37 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -31,7 +31,7 @@ DATABASE_PASSWORD = str(os.getenv('DATABASE_PASSWORD'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
